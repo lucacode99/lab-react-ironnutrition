df8831d94b40c3211a7697bbe74b6f8c12968907
Lucas Pfister <digitalservices@outlook.de>
2022-06-16 17:35:40 +0200

    working on iteration 1.3

--- src/App.js
+++ src/App.js
@@ -4,42 +4,62 @@ import React, {useState} from 'react'
 import FoodBox from './components/FoodBox'
 import AddFoodForm from './components/AddFoodForm'
 import foodsJSON from './foods.json'
+import Search from './components/Search'
+
 
 function App(props) {
-  //new
+  // add new food
   const [food, setFood] = useState(foodsJSON)
+  const [searchFood,setSearchFood]  = useState("")
   const addNewFood = (newFood) => {
       const updatedFood = [...food, newFood];
       setFood(updatedFood);
-    };
+  };
 
   return (
     <div className="App">
 
-      <Row>
-        <Col>
-          <Divider>Fancy Input</Divider>
-          <Input value={''} onChange={() => {}} />
-        </Col>
-
-        <Col>
-          <Card title='Fancy Card'>
-            <Button onClick={() => {}}>Fancy Button</Button>
-          </Card>
-        </Col>
-
-      </Row>
+      {/* add food */}
       <Col>
       <Card>
         <AddFoodForm addNewFood={addNewFood}/>
-        
-
       </Card>
     </Col>
-    <FoodBox newFood ={food}/>
+
+      
+    <Col>
+      <Card>
+        <Search searchFood={searchFood} onSearch={setSearchFood}/>
+        
+      </Card>
+    </Col>
+    {food
+        .filter((elem, i) => {
+          return elem.name.toLowerCase().includes(searchFood.toLowerCase())
+        }).map((elem, i) => {
+          
+          return (
+            
+            <col>
+            <Card
+              title={elem.name}
+              style={{ width: 230, height: 300, margin: 10}} 
+            >
+            <img src={elem.image} height={60} alt ="food" />
+            <p>Calories: {elem.calories}</p>
+            <p>Servings: {elem.servings}</p>
+            <p>Total Calories: {elem.calories * elem.servings}</p>
+            </Card>
+            </col>
+          )}
+         
+    )}  
+        
     
+        
+    <FoodBox newFood={food} />
+
     </div>
-  );
-}
+    )}
 
 export default App;
